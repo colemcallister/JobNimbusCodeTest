8ce1079c3985a88b45855ffaf5cad4931dc0a763
Cole McAllister <cole.mcallister@churchofjesuschrist.org>
2021-04-12 12:53:23 -0600

    Renamed a variable for better readability

--- src/main/kotlin/StringVerifier.kt
+++ src/main/kotlin/StringVerifier.kt
@@ -1,18 +1,18 @@
 
 class StringVerifier {
     fun haveMatchingBrackets(input: String): Boolean {
-        var numberOfOpenBrackets = 0
+        var numberOfOpenBracketsUnclosed = 0
 
         input.forEach { character ->
             if (character == '{') {
-                numberOfOpenBrackets++
-            } else if (character == '}' && numberOfOpenBrackets > 0) {
-                numberOfOpenBrackets--
+                numberOfOpenBracketsUnclosed++
+            } else if (character == '}' && numberOfOpenBracketsUnclosed > 0) {
+                numberOfOpenBracketsUnclosed--
             } else if (character == '}') {
                 return false
             }
         }
 
-        return numberOfOpenBrackets == 0
+        return numberOfOpenBracketsUnclosed == 0
     }
 }
